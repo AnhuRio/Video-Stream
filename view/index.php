26fbb65f7643ddfe5561cd946446741b22243591
AnhuRio <64960764+AnhuRio@users.noreply.github.com>
2023-01-15 16:11:20 +0000

    Add files via upload

---
+++ view/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="gamepage.css">
+  <title></title>
+  
+  <!-- The site uses Bootstrap v5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+  </script>
+</head>
+<body>
+  
+  <!-- overall container for page -->
+  <div class="container">
+
+    <!-- header -->
+    <div id="header">
+      <h1 class=text-center> CMP 408 Video Streaming Site </h1>
+    </div>
+    <!-- Header -->
+    <br />
+
+    <!-- Display all cards -->
+<div class="row">
+  <div class="col-sm-5">
+    <div class="card">
+       <img class="card-img-top" src="images/bunny.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Big Buck Bunny</h5>
+        <p class="card-text">Big Buck Bunny (code-named Project Peach) is a 2008 short computer-animated comedy film featuring animals of the forest, made by the Blender Institute, part of the Blender Foundation..</p>
+        <a href="https://d2l47neh3dl7ge.cloudfront.net/bbb_sunflower_1080p_60fps_normal.mp4" class="btn btn-primary">Watch</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-5">
+    <div class="card">
+             <img class="card-img-top" src="images/skeleton_dance.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">The Skeleton Dance</h5>
+        <p class="card-text">The Skeleton Dance is a 1929 Silly Symphony animated short subject produced and directed by Walt Disney and animated by Ub Iwerks. In the film, four human skeletons dance and make music around a spooky graveyard.</p>
+        <a href="https://d2l47neh3dl7ge.cloudfront.net/Silly%20Symphonies%20%20The%20Skeleton%20Dance.mp4" class="btn btn-primary">Watch</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- End of Row div -->
+    <div id="footer">
+      <p class=text-center> Created by Maksymilian Rzeszotarski | Student ID: 1901183</p>
+      <p class=text-center>This site is created and updated for an University Asessment and will not be used for any
+        commerical purposes.</p>
+    </div>
+  </div> <!-- container -->
+</body>
+
+</html>
---
+++ view/video_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <!-- The site uses Bootstrap v5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+  </script>
+</head>
+<body>
+  
+    
+    <title>Video Page</title>
+  </head>
+  <body>
+    <a class="btn btn-secondary" href="index.php" role="button">Return</a>
+  <video id="player" width="100%" height="auto" autoplay="autoplay" controls>
+  <source src="https://d2l47neh3dl7ge.cloudfront.net/bbb_sunflower_1080p_60fps_normal.mp4" type="video/mp4" codecs="avc1.42E01E, mp4a.40.2">
+</video>
+  </body>
+</html>
